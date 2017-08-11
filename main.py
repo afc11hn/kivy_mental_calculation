27321112d8df882392f8bb669e3e22c4eacca508
MChrisDE <mchris@outlook.de>
2017-08-11 13:45:37 +0200

    add comments

--- main.py
+++ main.py
@@ -1,34 +1,33 @@
+import os
+
+os.environ['KIVY_GL_BACKEND'] = 'angle_sdl2'  # opengl error fix for win10
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.popup import Popup
-from kivy.uix.button import Button
 from kivy.properties import StringProperty
 from random import randint
-import os
-
-os.environ['KIVY_GL_BACKEND'] = 'angle_sdl2'
 
 
 class Trainer(BoxLayout):
     text = StringProperty()
 
     def __init__(self, **kwargs):
-        super(Trainer, self).__init__(**kwargs)
-        self.new_exercise()
+        super(Trainer, self).__init__(**kwargs)  # init BoxLayout
+        self.new_exercise()  # first exercise
 
-    def check(self, input):
+    def check(self, textinput):
         try:
-            input = int(input)
-        except ValueError:
+            textinput = int(textinput)
+        except ValueError:  # when textinput is NaN
             popup = ValueErrorPopup(auto_dismiss=False)
             popup.open()
-        if isinstance(input, int):
+        if isinstance(textinput, int):  # test if int(textinput) worked
             try:
                 solution = eval(self.text)
             except Exception:
                 self.text = "Error"
 
-            if solution == input:
+            if solution == textinput:
                 self.new_exercise()
             else:
                 popup = TAPopup(auto_dismiss=False)
@@ -39,11 +38,16 @@ class Trainer(BoxLayout):
         n1 = randint(1, 100)
         n2 = randint(1, 100)
         self.text = str(n1) + "+" + str(n2)
+        # TODO new and better exercises. e.g. minus, multiply, divide, square, root or n1 + n2 - n3
+
 
 class ValueErrorPopup(Popup):
     pass
+
+
 class TAPopup(Popup):
     pass
+
 
 class MathApp(App):
     def build(self):
